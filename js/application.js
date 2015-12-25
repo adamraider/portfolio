78aa5beecc03c24494cf7c82ed797f9d31a3d74a
Adam Raider <adamjraider@gmail.com>
2015-12-24 20:48:05 -0500

    Better gulpfile, smooth scroll

---
+++ js/application.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+  var content = $(".jumbotron .content")
+  content.css({'opacity': 0, 'marginTop': '8px'})
+  content.animate({
+    'opacity': 1, 
+    'marginTop': '0px'
+  }, 600, "swing")
+
+  var i = 0
+  var rotator = $("ul#text_rotator")
+  var rotator_items_parent = $("ul#text_rotator li")
+  var rotator_items = $("ul#text_rotator li span")
+
+  startRotator()
+
+  function startRotator() {
+    setInterval(function() {
+      rotate()
+    }, 3400)
+  }
+  function rotate(){
+    if(i < rotator_items.length - 1) {
+      i++
+      current_item = rotator_items.eq(i)
+      parent_item = rotator_items_parent.eq(i)
+      rotator.animate({
+        'width' : 0
+      }, 500, "swing")
+      rotator.animate({
+        'marginTop' : '-=' + parent_item.outerHeight()
+      }, 100, "swing")
+      rotator.animate({
+        'width' : current_item.width()
+      }, 1400, "swing")
+    } else {
+      i = 0
+      current_item = rotator_items.eq(i)
+      rotator.animate({
+        'width' : 0
+      }, 500, "swing")
+      rotator.animate({
+        'marginTop' : '0',
+      }, 100, "swing")
+       rotator.animate({
+        'width' : current_item.width()
+      }, 1400, "swing")
+    }
+  }
+})
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
